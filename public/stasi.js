8c164817ea09a08f4818c1a7bcb7a83b8cd5343d
Sambhaji <bbi-m1017@BBI-Ms-MacBook-Pro.local>
2018-02-04 09:32:14 +0530

    Created layout, programmed for creating the files

--- public/stasi.js
+++ public/stasi.js
@@ -1,9 +1,43 @@
 $(document).ready(function() {
-    $("#addNewApp").on("click", function() {
+    $("#btnSaveApp").on("click", function() {
+        // Declare and initialize appDetails object
+        var appDetails = {};
+        appDetails.appName = document.getElementById("inputAppName").value;
+        appDetails.appVersion = document.getElementById("inputAppVersion").value;
+        appDetails.fileNumber = Date.now();
 
+        if (appDetails.appName !== "" && appDetails.appVersion !== "") {
+            // write the file here
+            createFile(appDetails);
+            // hide the modal
+            $("#addNewAppModal").modal("hide");
+        }
     });
 
     $("#save").on("click", function() {
 
     });
 });
+
+/**
+ * function creating the file and loading the apps list again
+ * @param {object} appDetails 
+ */
+function createFile(appDetails) {
+    var httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert("Cannot create XMLHttpRequest");
+        return false;
+    }
+
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                // reload the apps list here
+                console.log(httpRequest.responseText);
+            }
+        }
+    };
+    httpRequest.open("GET", "http://127.0.0.1:9000/create_file?appName=" + appDetails.appName + "&appVersion=" + appDetails.appVersion + "&fileNumber=" + appDetails.fileNumber, true);
+    httpRequest.send();
+}
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 // load the express module
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+
+// load the file system module
+var fileSys = require("fs");
 
 // use the static files from public folder
 app.use(express.static("public"));
 
 app.get("/index.html", function(request, response) {
     response.send(__dirname + "/index.html");
+});
+
+app.get("/create_file", function(request, response) {
+
+    var file = fileSys.writeFile("./public/stasi_files/" + request.query.fileNumber + ".json", "[]", function(err) {
+        if (err) {
+            console.error("Error in creating file");
+        } else {
+            console.log(request.query.appName + ", " + request.query.appVersion + ", " + request.query.fileNumber + " created successfully");
+        }
+    });
+
+    response.send(request.query.appName + ", " + request.query.appVersion + ", " + request.query.fileNumber);
 });
 
 var server = app.listen(9000, function() {
